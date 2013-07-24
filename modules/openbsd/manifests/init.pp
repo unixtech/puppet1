7977eec7952fe1bb3b2a5a03043404e17ab093bf
Abhay Trivedi <abhaytrivedi3@gmail.com>
2013-07-24 17:10:27 +0530

    All the files added

--- modules/openbsd/manifests/init.pp
+++ modules/openbsd/manifests/init.pp
@@ -3,9 +3,9 @@ class openbsd {
 		source => 'puppet:///modules/openbsd/zshrc_openbsd',
 #		notify => 'ZSHRC for openbsd has been deployed'
 }
-	file { '/root/.vimrc'	:	source => 'puppet:///modules/openbsd/vimrc1', owner => 'root', mode => 0644, }
+	file { '/root/.vimrc'	:	source => 'puppet:///modules/openbsd/vimrc', owner => 'root', mode => 0644, }
 	file { '/root/.oh-my-zsh'	: source => 'puppet:///modules/openbsd/oh-my-zsh', owner => 'root', ensure => directory, recurse => true, force => true, mode => 0644,  }
-	file { '/root/.vim'	: source => 'puppet:///modules/openbsd/files/vim', owner => 'root',  ensure => directory, recurse => true, force => true, mode => 0644,  }
+	file { '/root/.vim'	: source => 'puppet:///modules/openbsd/vim', owner => 'root',  ensure => directory, recurse => true, force => true, mode => 0644,  }
 
 
 	user {	'art':
