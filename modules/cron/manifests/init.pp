3b4d11488184ededf1d815dcf19ba9b740b184cb
Abhay Trivedi <abhaytrivedi3@gmail.com>
2013-09-19 07:31:48 +0530

    Cron job shortened

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -1,10 +1,10 @@
 class cron {
-    cron { 'Sync GIT Repo':
-        command => 'cd /home/git ; git fetch -u origin master',
-        hour => '*',
-        minute => '*/20',
-        user => 'git'
-      }
+  cron { 'Sync GIT Repo':
+    command => 'cd /home/git ; git fetch -u origin master',
+            hour => '*',
+            minute => '*/1',
+            user => 'git'
+  }
 }
 
 
