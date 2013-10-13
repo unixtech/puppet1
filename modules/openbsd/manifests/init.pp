96dc5ac7d346498d21c938290dd7f15d2a3ffa57
Abhay Trivedi <abhaytrivedi3@gmail.com>
2013-10-13 23:25:36 +0530

    Try fire1 new1

--- modules/openbsd/manifests/init.pp
+++ modules/openbsd/manifests/init.pp
@@ -1,11 +1,11 @@
 class openbsd {
-	file { '/root/.zshrc' :
-		source => 'puppet:///modules/openbsd/zshrc_openbsd',
-#		notify => 'ZSHRC for openbsd has been deployed'
-}
-	file { '/root/.vimrc'	:	source => 'puppet:///modules/openbsd/vimrc', owner => 'root',			}
-	file { '/root/.oh-my-zsh'	: source => 'puppet:///modules/openbsd/oh-my-zsh', owner => 'root', ensure => directory, recurse => true, force => true, mode => 0644,  }
-	file{ '/root/.vim'	: source => 'puppet:///modules/openbsd/vim', owner => 'root',  ensure => directory, recurse => true, force => true, mode => 0644,  }
+	#file { '/root/.zshrc' :
+		#source => 'puppet:///modules/openbsd/zshrc_openbsd',
+##		notify => 'ZSHRC for openbsd has been deployed'
+#}
+	#file { '/root/.vimrc'	:	source => 'puppet:///modules/openbsd/vimrc', owner => 'root',			}
+	#file { '/root/.oh-my-zsh'	: source => 'puppet:///modules/openbsd/oh-my-zsh', owner => 'root', ensure => directory, recurse => true, force => true, mode => 0644,  }
+	#file{ '/root/.vim'	: source => 'puppet:///modules/openbsd/vim', owner => 'root',  ensure => directory, recurse => true, force => true, mode => 0644,  }
 
 
 	user {	'art':
