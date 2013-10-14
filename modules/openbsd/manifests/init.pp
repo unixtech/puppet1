0a10ce0a257bd327bcd1c6878a8046c981882dc9
Abhay Trivedi <abhaytrivedi3@gmail.com>
2013-10-14 11:38:51 +0530

    Soory, PF needs to be deployed

--- modules/openbsd/manifests/init.pp
+++ modules/openbsd/manifests/init.pp
@@ -1,4 +1,14 @@
 class openbsd {
+
+  $OSFAM = $::osfamily? {
+    OpenBSD => 'wheel',
+    default => 'root'
+  }
+
+  $HOSTN = $::hostname? {
+    fire1 => 'puppet:///modules/openbsd/pf_fire1.conf',
+    fire2 => 'puppet:///modules/openbsd/pf_fire2.conf'
+  }
 	#file { '/root/.zshrc' :
 		#source => 'puppet:///modules/openbsd/zshrc_openbsd',
 ##		notify => 'ZSHRC for openbsd has been deployed'
@@ -6,7 +16,25 @@ class openbsd {
 	#file { '/root/.vimrc'	:	source => 'puppet:///modules/openbsd/vimrc', owner => 'root',			}
 	#file { '/root/.oh-my-zsh'	: source => 'puppet:///modules/openbsd/oh-my-zsh', owner => 'root', ensure => directory, recurse => true, force => true, mode => 0644,  }
 	#file{ '/root/.vim'	: source => 'puppet:///modules/openbsd/vim', owner => 'root',  ensure => directory, recurse => true, force => true, mode => 0644,  }
+  file{ '/etc/pkg.conf':
+    source => 'puppet:///modules/openbsd/pkg.conf',
+    owner => 'root',
+    group => $OSFAM,
+    ensure => 'present'
+    }
+  file{ '/etc/sysctl.conf':
+    source => 'puppet:///modules/openbsd/sysctl.conf',
+    owner => 'root',
+    group => $OSFAM,
+    ensure => 'present'
+    }
 
+  file{ '/etc/pf.conf':
+    source => $HOSTN,
+    owner => 'root',
+    group => $OSFAM,
+    ensure => 'present'
+    }
 
 	user {	'art':
 		ensure	=> present,
@@ -21,10 +49,22 @@ class openbsd {
 							'unrar',
               'openvpn',
               'pftop',
-              'pfstat'
+              'pfstat',
+              'unbound',
+              'nsd3',
+              'squid',
+              'vsftpd'
 	
 	]:
 	ensure => installed,
 	#notify => "packages $package has been installed on fire1"	
+		}
+
+
+		exec {'Check the diskspace and Report if back tryit':
+			command => '/bin/echo I ran this command on `date +%F` >>/tmp/exec_try.txt',
+      #creates => '/tmp/exec_try.txt'
+      path   => "/usr/bin:/usr/sbin:/bin",
+      onlyif => "test `cat /tmp/exec_try.txt | wc -l` -lt 100"
 		}
 }
